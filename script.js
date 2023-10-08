554faaf685417930606ce6d6f645db6f77b72274
Ayush singh <ayushsingh2632003@gmail.com>
2023-10-08 20:48:37 +0530

    final_commit

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-const accessKey = "KIAni-h0Dh8HvHFrkuNWK2mPgCJdxq8YaXHKLX2Mta0"
-const formE1 = document.querySelector("form")
-const inputE1 = document.getElementById("search-input")
-const searchResult = document.querySelector(".search-results")
-const showMore = document.getElementById("show-more-button")
- let inputData = ""
- let page =1;
+const accessKey = "KIAni-h0Dh8HvHFrkuNWK2mPgCJdxq8YaXHKLX2Mta0";
+const formE1 = document.querySelector("form");
+const inputE1 = document.getElementById("search-input");
+const searchResult = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+let inputData = "";
+let page = 1;
+ 
+async function searchImages() {
+  inputData = inputE1.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
- async function searchImages(){
-    inputData = inputE1.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+  const response = await fetch(url);
+  const data = await response.json();
 
-    const response =await fetch(url)
-    const data = await response.json()
+  const results = data.results;
 
-    const results = data.results
+  if (page === 1) {
+    searchResult.innerHTML = "";
+  }
 
-    if(page===1){
-        searchResult.innerHTML=""
-    }
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result"); // Change "search-results" to "search-result"
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
 
-    results.map((result)=>{
-        const imageWrapper=document.createElement('div');
-        imageWrapper.classList.add("search-results");
-        const image = document.createElementNS('img');
-        image.src = result.urls.small;
-        image.alt=result.alt_description;
-        const imageLink = document.createElement('a');
-        imageLink.href=result.links.html;
-        imageLink.target="_blank";
-        imageLink.textContent = result.alt_description;
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResult.appendChild(imageWrapper);
+  });
 
-        imageWrapper.appendChild(image);
-        imageWrapper.appendChild(imageLink);
-        searchResult.appendChild(imageWrapper);
+  page++;
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
+}
 
+formE1.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
+});
 
-    });
-
-    page++
-    if(page>1){
-        showMore.style.display = "block"
-    }
- }
- formE1.addEventListener("submit",(event)=>{
-    event.preventDefault()
-    page=1;
-    searchImages()
- })
- showMore.addEventListener("submit",()=>{
-    searchImages()
- })
+showMore.addEventListener("click", () => { // Change "submit" to "click"
+  searchImages();
+});
