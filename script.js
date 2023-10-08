8309e024097077fe182abca3966b7a69caddd585
Devraj Mudgal <devrajmudgal17@gmail.com>
2023-10-08 19:28:43 +0530

    added the basic functional  variables by DOM

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const accessKey = "KIAni-h0Dh8HvHFrkuNWK2mPgCJdxq8YaXHKLX2Mta0"
 const formE1 = document.querySelector("form")
+const input = document.getElementById("search-input")
+const searchResult = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
